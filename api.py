a4a777fc62f21f2885ccf71e0930f0d003467ca0
stanescuV <stanescuvictor12@gmail.com>
2026-03-09 13:58:00 +0100

    mail recived bot working properly

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ import httpx
 from fastapi import FastAPI, Request
 from script import run_code_review_with_tools
 
-# PRODUCTION PASSWORD : KEY155115522633@
 app = FastAPI()
 
 @app.post("/webhook")
